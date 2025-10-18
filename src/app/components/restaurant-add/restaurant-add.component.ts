33fc6af7c6fe5d322cbde96f139577ef6931a770
Logan <lbpotopn@lakeheadu.ca>
2025-10-18 14:56:07 -0400

    Added new components to project, plus deleted user component

---
+++ src/app/components/restaurant-add/restaurant-add.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurant-add',
+  imports: [],
+  templateUrl: './restaurant-add.component.html',
+  styleUrl: './restaurant-add.component.css'
+})
+export class RestaurantAddComponent {
+
+}
