c3b98f836870a237aa88afaada41cad5d83a9498
Logan <lbpotopn@lakeheadu.ca>
2025-10-18 15:35:28 -0400

    Added user component back in, updated nav and app html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Import all the components used as pages
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { RegisterComponent } from './components/register/register.component';
+import { RestaurantComponent } from './components/restaurant/restaurant.component';
+import { RestaurantAddComponent } from './components/restaurant-add/restaurant-add.component';
+import { UserComponent } from './components/user/user.component';
+
+
+
+export const routes: Routes = [
+    {path: 'restaurant', component: RestaurantComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'logout', component: LogoutComponent},
+    {path: '', component: HomeComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'restaurant-add', component: RestaurantAddComponent},
+    {path: 'user', component: UserComponent}
+
+  
+  // You probably won't route to nav directly, since it appears on all pages
+];
