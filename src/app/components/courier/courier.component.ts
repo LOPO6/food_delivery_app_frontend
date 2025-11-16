4ef3a5df2cbc0f580a4696ba85e6eeb634390a8b
Logan <lbpotopn@lakeheadu.ca>
2025-11-15 21:00:55 -0500

    Added empty components for courier and payment pages, changed favicon to company logo

---
+++ src/app/components/courier/courier.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-courier',
+  imports: [],
+  templateUrl: './courier.component.html',
+  styleUrl: './courier.component.css'
+})
+export class CourierComponent {
+
+}
