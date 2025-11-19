7029d5caefb4a4e197bcc97040a5c7e5b9de4ed3
Logan <lbpotopn@lakeheadu.ca>
2025-11-19 15:25:37 -0500

    Added html and css to courier page, updated to do list

--- src/app/components/courier/courier.component.ts
+++ src/app/components/courier/courier.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-courier',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './courier.component.html',
   styleUrl: './courier.component.css'
 })
 export class CourierComponent {
+  activeTab: string = 'current-order';
 
+  //function that is called when the user clicks on a button
+  setTab(tab: string) {
+    this.activeTab = tab;
+  }
+  
 }
