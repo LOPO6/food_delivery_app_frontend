99f3e11f0b72a6e221e3eee71c049bda33ff2408
Conner <ckrecosk@lakeheadu.ca>
2025-11-21 23:27:12 -0500

    Added build script

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,5 @@
 export const environment = {
   production: false,
   serverUrl: 'http://localhost:3000/api',
-  
-  // This gets auto populated from env file when you run npm start
-  GOOGLE_MAPS_KEY: ''
+  GOOGLE_MAPS_KEY: 'AIzaSyBVrU4OpES6FW8prMGSoEHdkbBODUHscFc'
 };
